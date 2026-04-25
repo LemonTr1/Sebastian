b3514f7d3d073f3fe8e7b242678a5ab50133d9c0
LemonTr1 <13075537698@163.com>
2026-04-25 18:59:25 +0800

    rm_file工具编写/优化提示词

---
+++ Sebastian/Tools/File_Tools/rm_file.py
@@ -0,0 +1,40 @@
+import shutil
+
+import typer
+from agents import function_tool
+import os
+
+@function_tool
+def rm(path: str, filename: str)->bool:
+    """
+    用于删除某一路径下的文件或文件夹
+    Args:
+        path: 文件系统对象所在路径
+        filename: 文件系统对象名称
+    Returns:
+        True/False: 是否成功删除文件系统对象
+    """
+    file_path = os.path.join(path, filename)
+    if not os.path.exists(file_path):
+        typer.echo(typer.style("[Warn]文件或路径不存在",fg=typer.colors.YELLOW))
+        return False
+    confirmed = typer.confirm(f"确定要删除文件：{file_path}吗？")
+    if not confirmed:
+        return False
+
+    try:
+        if os.path.isfile(file_path):
+            #删除文件
+            os.remove(file_path)
+            print(f"文件已删除: {file_path}")
+        elif os.path.isdir(file_path):
+            #删除文件夹
+            shutil.rmtree(file_path)
+            print(f"文件夹已删除: {file_path}")
+    except PermissionError:
+        print(f"权限不足，无法删除: {file_path}")
+        return False
+    except Exception as e:
+        print(f"删除失败: {e}")
+        return False
+    return True
